ea5d7bb3ec478416f990f152bb1ee789ad82e4c0
masoudmard <mardmasoud66gmail.com>
2022-12-30 15:47:09 +0330

    up1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-
-import { SeasonDisplay } from "./SeasonDisplay";
-
-function App() {
- 
+import React, { Component } from 'react'
+export default class App extends Component {
   
- 
-
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } 
+  constructor(props){
+    super(props)
+    this.state = {lat:null,error:""} 
+    navigator.geolocation.getCurrentPosition(pos => this.setState({lat:pos.coords.latitude}
+      ,err=>this.setState({error:err.message})
+      )
+       
+    );
+  }
+  render() {
+    return (
+      <div>
+      <p>Latitude:{this.state.lat}</p>   
+      <p>error:{this.state.error}</p>
+      </div>
+      
+      
+    )
+  }
 }
 
-function showPosition(position) {
-console.log("Latitude: " + position.coords.latitude + 
-  "\nLongitude: " + position.coords.longitude);
-}
-  
-getLocation() 
-  return (
-    <div>
-     season app
-<SeasonDisplay/>
-    </div>
-  );
-}
 
-export default App;
+
